ae88aa0a7727e75e089d57d0346b09f39eb03c0d
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2024-09-19 17:28:01 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,35 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  //excusa1 = ["el perro se perdio en el bosque"]
+  //excusaEjemplo = "Who action place"
+
+  //arrays con las excusas que queremos mostrar
+
+  let who = ["El gato", "la niñera", "La abuela", "El pez", "El niño"];
+  let action = [
+    "se comio un pan",
+    "pisó al gato",
+    "dibujó un mar",
+    "se murió",
+    "estudió matematicas"
+  ];
+  let place = [
+    "en el bosque",
+    "en la casa",
+    "en el metro",
+    "en la piscina",
+    "en el avión"
+  ];
+
+  //debemos generar numeros random para esas excusas
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomPlace = Math.floor(Math.random() * place.length);
+
+  //debemos unir estos dos conceptos para que sean mostrados en la ventada.
+  document.querySelector("#excuse").innerHTML =
+    who[randomWho] + " " + action[randomAction] + " " + place[randomPlace];
 };
